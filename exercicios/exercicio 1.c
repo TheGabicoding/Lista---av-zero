e7cff5c4a056d3202ad24b69f1575fcaa959f113
Gabriel Augusto <gabriel23gomes11@gmail.com>
2026-03-04 11:48:12 -0300

    exercicio 1 feito.

--- exercicios/exercicio 1.c
+++ exercicios/exercicio 1.c
@@ -1,1 +1,15 @@
+//atividade Impressão de Números de 1 a N
+#include <stdio.h>
+int i;
+int n;
 
+int main(void) { 
+printf("Digite um numero inteiro positivo: ");
+    scanf("%d", &n);
+  
+    for (i = 1; i <= n; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
